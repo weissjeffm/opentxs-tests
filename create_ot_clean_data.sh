71594f524ccd24c18cc8268501835d9ca36d843f
benma <mbencun@gmail.com>
2014-10-31 10:05:58 +0100

    Create fresh data before running the tests.

--- create_ot_clean_data.sh
+++ create_ot_clean_data.sh
@@ -7,15 +7,14 @@
 # current dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
 OT_FOLDER=~/.ot
+
 OT_TARGET_FOLDER=$DIR/ot-clean-data
 
 rm -rf $OT_FOLDER
 # workaround for currently broken `opentxs changepw`
 opentxs --dummy-passphrase changepw
-(cd python3 && python3 -c "from pyopentxs import server; server.setup(open('../test-data/sample-contracts/localhost.xml'))" | opentxs-notary --only-init)
-
+(cd $DIR/python3 && python3 -c "from pyopentxs import server; server.setup(open('../test-data/sample-contracts/localhost.xml'))" | opentxs-notary --only-init)
 
 # delete folder if it exists
 if [ -d "$DIR/ot-clean-data/.ot" ]; then
