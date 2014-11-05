cc257c9208ebf44b7d68633fe1150ef32551602d
Jeff Weiss <jeff.weiss@monetas.net>
2014-11-05 07:26:01 -0500

    Do server setup in python only, remove shell script
      Can now define multiple notaries for the client,
      even though it will only really communicate with one
      allows tests for "wrong server"

--- python3/runtests.py
+++ python3/runtests.py
@@ -6,18 +6,30 @@ import sys
 import time
 import pytest
 import pyopentxs
+from pyopentxs import notary
+import subprocess
+
+temp_data_dir = "../ot-clean-data/.ot"
 
 
 def create_fresh_ot_config():
     # this creates fresh data in ../ot-clean-data/.ot
-    os.system("../create_ot_clean_data.sh")
+    # os.system("../create_ot_clean_data.sh")
+    shutil.rmtree(pyopentxs.config_dir)
+    os.system("opentxs --dummy-passphrase changepw")
+    setup_data = notary.setup(open('../test-data/sample-contracts/localhost.xml'))
+    p = subprocess.Popen(["opentxs-notary", "--only-init"], stdin=subprocess.PIPE)
+    outs, errs = p.communicate(input=setup_data.getvalue(), timeout=20)
+    if os.path.exists(temp_data_dir):
+        shutil.rmtree(temp_data_dir)
+    shutil.move(pyopentxs.config_dir, temp_data_dir)
 
 
 def install_ot_config():
     # copy the clean data directory
     if os.path.exists(pyopentxs.config_dir):
         shutil.rmtree(pyopentxs.config_dir)
-    shutil.copytree("../ot-clean-data/.ot", pyopentxs.config_dir)
+    shutil.copytree(temp_data_dir, pyopentxs.config_dir)
 
 
 def restart_opentxs_notary():
