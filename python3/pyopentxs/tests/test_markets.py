a21aed69f775c97dde1157bbaf5a017fd97c20ee
Jeff Weiss <jeff.weiss@monetas.net>
2014-12-15 13:39:56 -0500

    Fixed assumption that market would be at index 0
    also fixed lint errors

--- python3/pyopentxs/tests/test_markets.py
+++ python3/pyopentxs/tests/test_markets.py
@@ -63,22 +63,30 @@ def test_ask_market_price(marketaccounts):
     assert(bob.account1.balance() == 104)
     assert(bob.account2.balance() == 69)
 
+
 def test_market_offers_buying(marketaccounts):
     alice = marketaccounts.alice
     bob = marketaccounts.bob
     pyopentxs.otme.create_market_offer(bob.account1._id, bob.account2._id,
                                        1, 1, 3, 7, False, 10000, "", 0)
-    
+
     time.sleep(cron_interval)
     server_id = bob.account1.server_id
 
     message = pyopentxs.otme.get_market_list(server_id, alice.account1.nym._id)
     assert pyopentxs.is_message_success(message)
 
-    obj = opentxs.QueryObject(opentxs.STORED_OBJ_MARKET_LIST, "markets", server_id, "market_data.bin")
-    marketList = opentxs.MarketList_ot_dynamic_cast(obj)
-    assert 1 == marketList.GetMarketDataCount()
-    c = marketList.GetMarketData(0)
+    obj = opentxs.QueryObject(opentxs.STORED_OBJ_MARKET_LIST, "markets",
+                              server_id, "market_data.bin")
+    market_list = opentxs.MarketList_ot_dynamic_cast(obj)
+    market_data_count = market_list.GetMarketDataCount()
+    assert market_data_count >= 1
+    market_data = [market_list.GetMarketData(m) for m in range(market_data_count)]
+    matching_markets = list(filter(lambda m: (bob.account1.asset._id == m.instrument_definition_id
+                                              and bob.account2.asset._id == m.currency_type_id),
+                                   market_data))
+    assert len(matching_markets) == 1
+    c = matching_markets[0]
 
     assert bob.account1.asset._id == c.instrument_definition_id
     assert bob.account2.asset._id == c.currency_type_id
@@ -96,12 +104,11 @@ def test_market_offers_buying(marketaccounts):
     assert '0' == c.recent_lowest_ask
     assert '' == c.last_sale_date
 
-    market_id = c.market_id
-
     message = pyopentxs.otme.get_market_offers(server_id, alice.account1.nym._id, c.market_id, 20)
     assert pyopentxs.is_message_success(message)
 
-    obj = opentxs.QueryObject(opentxs.STORED_OBJ_OFFER_LIST_MARKET, "markets", server_id, c.market_id + ".bin")
+    obj = opentxs.QueryObject(opentxs.STORED_OBJ_OFFER_LIST_MARKET, "markets",
+                              server_id, c.market_id + ".bin")
     tradeList = opentxs.OfferListMarket_ot_dynamic_cast(obj)
     assert 0 == tradeList.GetAskDataCount()
     assert 1 == tradeList.GetBidDataCount()
@@ -115,22 +122,31 @@ def test_market_offers_buying(marketaccounts):
     assert '3' == c.available_assets
     assert '1' == c.minimum_increment
 
+
 def test_market_offers_selling(marketaccounts):
     alice = marketaccounts.alice
     bob = marketaccounts.bob
     pyopentxs.otme.create_market_offer(bob.account1._id, bob.account2._id,
                                        1, 1, 3, 7, True, 10000, "", 0)
-    
+
     time.sleep(cron_interval)
     server_id = bob.account1.server_id
 
     message = pyopentxs.otme.get_market_list(server_id, alice.account1.nym._id)
     assert pyopentxs.is_message_success(message)
 
-    obj = opentxs.QueryObject(opentxs.STORED_OBJ_MARKET_LIST, "markets", server_id, "market_data.bin")
-    marketList = opentxs.MarketList_ot_dynamic_cast(obj)
-    assert 1 == marketList.GetMarketDataCount()
-    c = marketList.GetMarketData(0)
+    obj = opentxs.QueryObject(opentxs.STORED_OBJ_MARKET_LIST, "markets", server_id,
+                              "market_data.bin")
+
+    market_list = opentxs.MarketList_ot_dynamic_cast(obj)
+    market_data_count = market_list.GetMarketDataCount()
+    assert market_data_count >= 1
+    market_data = [market_list.GetMarketData(m) for m in range(market_data_count)]
+    matching_markets = list(filter(lambda m: (bob.account1.asset._id == m.instrument_definition_id
+                                              and bob.account2.asset._id == m.currency_type_id),
+                                   market_data))
+    assert len(matching_markets) == 1
+    c = matching_markets[0]
 
     assert bob.account1.asset._id == c.instrument_definition_id
     assert bob.account2.asset._id == c.currency_type_id
@@ -151,7 +167,8 @@ def test_market_offers_selling(marketaccounts):
     message = pyopentxs.otme.get_market_offers(server_id, alice.account1.nym._id, c.market_id, 20)
     assert pyopentxs.is_message_success(message)
 
-    obj = opentxs.QueryObject(opentxs.STORED_OBJ_OFFER_LIST_MARKET, "markets", server_id, c.market_id + ".bin")
+    obj = opentxs.QueryObject(opentxs.STORED_OBJ_OFFER_LIST_MARKET, "markets", server_id,
+                              c.market_id + ".bin")
     tradeList = opentxs.OfferListMarket_ot_dynamic_cast(obj)
     assert 1 == tradeList.GetAskDataCount()
     assert 0 == tradeList.GetBidDataCount()
