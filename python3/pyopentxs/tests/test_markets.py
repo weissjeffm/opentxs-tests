a9053fbc7ad70de085bd0facec748e88aebd7ad6
Jeff Weiss <jeff.weiss@monetas.net>
2014-12-30 08:48:19 -0500

    Read/write server config
       Also set cron interval to 2500ms - results in 10% cpu use but speeds
       up tests. Should look into fixing this in the notary

--- python3/pyopentxs/tests/test_markets.py
+++ python3/pyopentxs/tests/test_markets.py
@@ -4,9 +4,11 @@ import pytest
 import time
 import opentxs
 from pyopentxs import market
+from pyopentxs import notary
 
-cron_interval = 10
-
+notary.config.read()
+cron_interval = int(int(notary.config.parser['cron']['ms_between_cron_beats']) / 1000)
+print("set cron interval to %s" % cron_interval)
 
 @pytest.fixture
 def marketaccounts():
